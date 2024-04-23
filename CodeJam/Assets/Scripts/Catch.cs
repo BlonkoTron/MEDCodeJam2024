79dfaef43bada6a15d0aef94e76a818e08e4a936
gigrove <147515425+gigrove@users.noreply.github.com>
2024-04-23 11:41:41 +0200

    Added Catch classes + children; started FishManager

---
+++ CodeJam/Assets/Scripts/Catch.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public abstract class Catch 
+{
+    
+    public string name;
+    public int score;
+    public int difficulty;
+    //private GameObject self;
+
+    private void Initialise()
+    {
+        
+
+    }
+}
