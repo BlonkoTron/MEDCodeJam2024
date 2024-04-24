4680b1f129a0c96fdc46a24c1fa45bedc60556a4
ContaDoge <mgerst23@student.aau.dk>
2024-04-24 14:15:49 +0200

    s

--- CodeJam/Assets/Scripts/Vibrator.cs
+++ CodeJam/Assets/Scripts/Vibrator.cs
@@ -44,18 +44,4 @@ public class Vibrator : MonoBehaviour
         return false;
 #endif
     }
-    /* When using Amp it dosent work in the build version ;(
-public static void Vibrate(long milliseconds = 250, int amplitude = 255) // Vibrate for a given number of milliseconds with a given amplitude
-{
-    if (isAndroid())
-    {
-        vibrator.Call("vibrate", milliseconds, amplitude);
-        Debug.Log(Debug.isDebugBuild ? milliseconds.ToString() : "");
-    }
-    else
-    {
-        Handheld.Vibrate(); 
-    }
-}
-*/
 }
