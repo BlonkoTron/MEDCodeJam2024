6fab0142d79be547b623ff08292a9b21c6e891b6
ContaDoge <mgerst23@student.aau.dk>
2024-04-24 10:55:19 +0200

    Amp

--- CodeJam/Assets/Scripts/Tester.cs
+++ CodeJam/Assets/Scripts/Tester.cs
@@ -3,16 +3,28 @@ using UnityEngine.UI;
 
 public class Tester : MonoBehaviour
 {
-    public void Beginvibration()
+    public void Beginvibration200()
     {
-        Vibrator.Vibrate(500, 150); // Vibrate for 500 milliseconds with amplitude 150
+        Vibrator.Vibrate(1000, 200); // Vibrate for 1 second with an amplitude of 200
     }
-    public void BeginLongVibration()
+    public void Beginvibration255()
     {
-        Vibrator.Vibrate(10000, 255); // vibrate for 10 second with amplitude 255
+        Vibrator.Vibrate(1000, 255); // Vibrate for 1 second with the strongest amplitude
     }
     public void Stopvibration()
     {
         Vibrator.Cancel(); // Stop the vibration
+    }
+    public void Beginvibration100()
+    {
+        Vibrator.Vibrate(1000, 100); // Vibrate for 1 second with a weak amplitude
+    }
+    public void Beginvibration50()
+    {
+        Vibrator.Vibrate(1000, 50); // Vibrate for 1 second with a very weak amplitude
+    }
+    public void Beginvibration150()
+    {
+        Vibrator.Vibrate(1000, 150); // Vibrate for 1 second with a medium amplitude
     }
 }
