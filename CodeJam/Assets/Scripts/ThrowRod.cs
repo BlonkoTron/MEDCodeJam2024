e69140e12612f210a289f4889d9b4fc89ebbb00a
VHSTapeDVD <axschoenning@gmail.com>
2024-05-26 12:16:53 +0200

    Update ThrowRod.cs

--- CodeJam/Assets/Scripts/ThrowRod.cs
+++ CodeJam/Assets/Scripts/ThrowRod.cs
@@ -12,14 +12,12 @@ private bool isThrown = false;
 private float ignoreInputUntil = 0f;
 private float ignoreInputDuration = 1f; // Ignore input for 1 second
 
-private float ignoreInputInventory = 3f;
 public static ThrowRod instance;
 
 public delegate void UsingRod();
 
 public event UsingRod OnUsingRod;
 
-public GameObject inventory;
 public event UsingRod OnPullRod;
 
 void Awake()
@@ -52,62 +50,23 @@ void Update()
         Debug.Log("Pulled in!");
         ignoreInputUntil = Time.time + ignoreInputDuration; // Set the time until which to ignore input
     }
-    else if (inventory = null)
+    if (Input.GetKeyDown(KeyCode.T) && !isThrown)
     {
-        ignoreInputUntil = Time.time + ignoreInputInventory; // Set the time until which to ignore input
+        OnUsingRod?.Invoke();
+        Debug.Log("Thrown!");
+        isThrown = true;
+        ignoreInputUntil = Time.time + ignoreInputDuration; // Set the time until which to ignore input
     }
-
-        if (Input.GetKeyDown(KeyCode.T) && !isThrown)
+    else if (Input.GetKeyDown(KeyCode.T) && isThrown)
         {
-            OnUsingRod?.Invoke();
-            Debug.Log("Thrown!");
-            isThrown = true;
-            ignoreInputUntil = Time.time + ignoreInputDuration; // Set the time until which to ignore input
-        }
-        else if (Input.GetKeyDown(KeyCode.T) && isThrown)
-        {
-            isThrown = false;
-            OnPullRod?.Invoke();
-            Debug.Log("Pulled in!");
-            ignoreInputUntil = Time.time + ignoreInputDuration; // Set the time until which to ignore input
-        }
-        else if (inventory = null)
-        {
-            ignoreInputUntil = Time.time + ignoreInputInventory; // Set the time until which to ignore input
-        }
+        isThrown = false;
+        OnPullRod?.Invoke();
+        Debug.Log("Pulled in!");
+        ignoreInputUntil = Time.time + ignoreInputDuration; // Set the time until which to ignore input
+    }
 
 }
 
 
-IEnumerator WaitAndMove()
-{
-    // Wait for a random amount of time between 1 and 20 seconds before starting the time slot
-    yield return new WaitForSeconds(Random.Range(1, 10));
-    Debug.Log("The Fish is here!");
-    // Start the time slot
-    float startTime = Time.time;
-    float timeSlotDuration = 3.0f; // Set the duration of the time slot here
-
-    // Wait until the end of the time slot
-    while (Time.time - startTime < timeSlotDuration)
-    {
-         // Check if the action has been performed
-        if (isThrown == false)
-        {
-            Debug.Log("Pulled at the right time!");
-            yield break; // Stop the coroutine here
-        }
-    yield return null; // Wait for the next frame
-    }
-
-    // If the action wasn't performed during the time slot
-    if (isThrown)
-    {
-        Debug.Log("Pulled too late!");
-        yield break; // Stop the coroutine here
-        // Add your code here to handle the end of the time slot without action
-    }
-  
-}
 
 }
